855025cefe3197bb3f1585726cf470f8caba455f
VivekMahapatra <61105157+VivekMahapatra@users.noreply.github.com>
2024-07-10 22:12:38 -0500

    Added Queue Code for Blaze and Neurologic
    Work on this. Instructions inside. Will post testing code tmrw

---
+++ ds/queue.py
@@ -0,0 +1,74 @@
+""" You are an owner of a hot dog stand (vegetarian hot dogs for Blaze and GameBot). """
+""" All of a sudden aliens take over the planet and capture the entire planet. They """
+""" are about to destroy your hot dog stand when they notice how good your (vegetarian)"""
+""" hot dogs are. Because of this, they make a bet with you: Simulate your hotdog stand """
+""" business using python code, and if you pass, humanity will be saved. Because you're """
+""" an overachiever (nerd), you want to implement this not only by modifying your old """
+""" stack code but ALSO USING AN IMMUTEABLE ARRAY OF SIZE 10. Good luck, the fate of """
+""" humanity lies in your hands."""
+
+class PersonStack:
+
+    def __init__(self,name):
+        self.name = name
+
+class StoreLineStack:
+
+    def __init__(self):
+
+        """ implement your stack here """
+
+        pass
+
+    def enqueue(self, new_name):
+
+        """ Rewrite one of your stack functions to work here """
+
+        pass
+
+
+    def dequeue(self):
+
+        """ Rewrite one of your stack functions to work here """
+        """ ALSOOO Modify the dequeue so it returns the person that got dequeued """
+        """ Should return a Person type """
+
+
+        pass
+
+    def peek(self):
+
+        """ NEW FUNCTION see if you can modify your stack peek code to see whos at the front of the line """
+        """ Should return a Person type """
+
+        pass
+
+
+class StoreLineArray:
+
+    """ HEHEHEHEHEHEHEHE Challenge Mode """
+
+    def __init__(self):
+        self.array = [None] * 10                #instantiate empty list of size 10 DO NOT CHANGE THE SIZE
+
+    def enqueue(self):
+
+        """ Write Dis """
+        
+        pass
+
+    def dequeue(self):
+        
+        """ Write Dis """
+
+        pass
+
+    def peek(self):
+
+        """ Write Dis """
+
+        pass
+
+    
+
+    
