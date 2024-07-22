f7ed0f16ef34d94c0f27eba057a92d0aed300916
VivekMahapatra <61105157+VivekMahapatra@users.noreply.github.com>
2024-07-21 21:08:42 -0500

    Update queue.py with LL thingies, better comments

--- ds/queue.py
+++ ds/queue.py
@@ -1,12 +1,14 @@
 from typing import Generic, TypeVar
+from stack import GenericStack as Stack
+from linked_list import GenericLinkedList as LinkedList
 
 """ You are an owner of a hot dog stand (vegetarian hot dogs for Blaze and GameBot).
 All of a sudden aliens take over the planet and capture the entire planet. They
 are about to destroy your hot dog stand when they notice how good your (vegetarian)
 hot dogs are. Because of this, they make a bet with you: Simulate your hotdog stand
 business using python code, and if you pass, humanity will be saved. Because you're
-an overachiever (nerd), you want to implement this not only by modifying your old
-stack code but ALSO USING AN IMMUTEABLE ARRAY OF SIZE 10 AND USING TWO STACKS. 
+an overachiever (nerd), you want to implement this not only by using a Linked List
+but ALSO USING AN IMMUTEABLE ARRAY OF SIZE 10 AND USING TWO STACKS. 
 Good luck, the fate of humanity lies in your hands.
 """
 
@@ -15,34 +17,33 @@ class Person(Generic[T]):
 
     def __init__(self,name) -> None:
         self.name = name
+        
 
 
-class StoreLineStack(Generic[T]):
+class StoreLineLL(Generic[T]):
 
     def __init__(self) -> None:
 
-        """ implement your stack here """
+        self.list = LinkedList[T]()
 
-        pass
 
     def enqueue(self, new_name: T) -> None:
 
-        """ Rewrite one of your stack functions to work here """
+        """ Write an enqueue that works with linked lists """
 
         pass
 
 
     def dequeue(self) -> T:
 
-        """ Rewrite one of your stack functions to work here
-        ALSOOO Modify the dequeue so it returns the person that got dequeued
+        """ Write a dequeue that works with LL
         Should return a Person """
 
         pass
 
     def peek(self) -> T:
 
-        """ NEW FUNCTION see if you can modify your stack peek code to see whos at the front of the line
+        """ NEW FUNCTION see if you can add a peek for LL to see whos at the front of the line
         Should return a Person """
 
         pass
@@ -52,8 +53,12 @@ class StoreLineArray(Generic[T]):
 
     """ HEHEHEHEHEHEHEHE Challenge Mode """
 
-    def __init__(self) -> None:
-        self.array = [None] * 10          #instantiate empty list of size 10 DO NOT CHANGE THE SIZE
+    def __init__(self, size: int = 10) -> None:
+        self.array = [None] * size          
+
+    """instantiate empty list of size 10 DO NOT CHANGE THE SIZE
+    think about how you can keep track of the front and the rear 
+    of the queue without changing the size of the array"""
 
     def enqueue(self, new_name: T) -> None:
 
@@ -81,13 +86,12 @@ class StoreLineDoubleStack(Generic[T]):
     """
 
     def __init__(self) -> None:
+        self.stack1 = Stack[T]()
+        self.stack2 = Stack[T]()
 
-        """ Write Dis """
-
-        pass
             
     def enqueue(self, new_name: T) -> None:
-        
+
         """ Write Dis """
             
         pass
