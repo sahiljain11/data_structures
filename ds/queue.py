cca79c8d19a0c2ab8f4f459d918e70f238adab2c
sahiljain11 <sahilbot1@gmail.com>
2024-07-23 18:55:25 -0500

    Added improvements to comments and fixed generic

--- ds/queue.py
+++ ds/queue.py
@@ -2,87 +2,92 @@ from typing import Generic, TypeVar
 from stack import GenericStack as Stack
 from linked_list import GenericLinkedList as LinkedList
 
-""" You are an owner of a hot dog stand (vegetarian hot dogs for Blaze and GameBot).
-All of a sudden aliens take over the planet and capture the entire planet. They
-are about to destroy your hot dog stand when they notice how good your (vegetarian)
-hot dogs are. Because of this, they make a bet with you: Simulate your hotdog stand
-business using python code, and if you pass, humanity will be saved. Because you're
-an overachiever (nerd), you want to implement this not only by using a Linked List
-but ALSO USING AN IMMUTEABLE ARRAY OF SIZE 10 AND USING TWO STACKS. 
-Good luck, the fate of humanity lies in your hands.
+"""
+    You are an owner of a hot dog stand (vegetarian hot dogs for Blaze and GameBot).
+    All of a sudden aliens take over the planet and capture the entire planet. They
+    are about to destroy your hot dog stand when they notice how good your (vegetarian)
+    hot dogs are. Because of this, they make a bet with you: Simulate your hotdog stand
+    business using python code, and if you pass, humanity will be saved. Because you're
+    an overachiever (nerd), you want to implement this not only by using a Linked List
+    but ALSO USING AN IMMUTEABLE ARRAY OF SIZE 10 AND USING TWO STACKS. 
+    Good luck, the fate of humanity lies in your hands.
 """
 
 T = TypeVar('T')
-class Person(Generic[T]):
-
-    def __init__(self,name) -> None:
-        self.name = name
-        
 
 
-class StoreLineLL(Generic[T]):
+class QueueLinkedList(Generic[T]):
 
     def __init__(self) -> None:
-
         self.list = LinkedList[T]()
 
 
-    def enqueue(self, new_name: T) -> None:
-
-        """ Write an enqueue that works with linked lists """
-
+    def enqueue(self, element: T) -> None:
+        """
+            This function should add the given element into the end of the queue.
+            This should be O(1)
+        """
         pass
 
 
     def dequeue(self) -> T:
-
-        """ Write a dequeue that works with LL
-        Should return a Person """
+        """
+            This function should return the next element in the queue
+            If there is no element, throw an error. This should be O(1)
+        """
 
         pass
 
     def peek(self) -> T:
-
-        """ NEW FUNCTION see if you can add a peek for LL to see whos at the front of the line
-        Should return a Person """
-
+        """
+            This function should return the next element in the queue.
+            This should NOT remove any elements in the queue.
+            If there is no element, throw an error. This should be O(1)
+        """
         pass
 
 
-class StoreLineArray(Generic[T]):
-
-    """ HEHEHEHEHEHEHEHE Challenge Mode """
+class QueueArray(Generic[T]):
 
     def __init__(self, size: int = 10) -> None:
+        """
+            instantiate empty list of size 10 DO NOT CHANGE THE SIZE
+            think about how you can keep track of the front and the rear 
+            of the queue without changing the size of the array
+        """
         self.array = [None] * size          
 
-    """instantiate empty list of size 10 DO NOT CHANGE THE SIZE
-    think about how you can keep track of the front and the rear 
-    of the queue without changing the size of the array"""
 
-    def enqueue(self, new_name: T) -> None:
-
-        """ Write Dis """
-        
+    def enqueue(self, element: T) -> None:
+        """
+            This function should add the given element into the end of the queue.
+            If there is no space left, throw an error! This should be O(1)
+        """
         pass
 
     def dequeue(self) -> T:
-        
-        """ Write Dis """
+        """
+            This function should return the next element in the queue
+            If there is no element, throw an error. This should be O(1)
+        """
 
         pass
 
     def peek(self) -> T:
-
-        """ Write Dis """
-
+        """
+            This function should return the next element in the queue.
+            This should NOT remove any elements in the queue.
+            If there is no element, throw an error. This should be O(1)
+        """
         pass
     
-class StoreLineDoubleStack(Generic[T]):
+class QueueDoubleStack(Generic[T]):
 
     """ CHALLENGE MODE PART 2
-    Rather than modifying our stack functions like we did above,
-    How can we implement a queue using 2 Stacks without modifying the stack funcs?
+        Rather than modifying our stack functions like we did above,
+        How can we implement a queue using 2 Stacks without modifying the stack funcs?
+
+        This is considered a bonus and OPTIONAL!!
     """
 
     def __init__(self) -> None:
@@ -90,25 +95,42 @@ class StoreLineDoubleStack(Generic[T]):
         self.stack2 = Stack[T]()
 
             
-    def enqueue(self, new_name: T) -> None:
-
-        """ Write Dis """
-            
+    def enqueue(self, element: T) -> None:
+        """
+            This function should add the given element into the end of the queue.
+            If there is no space left, throw an error! This should be O(1)
+        """
         pass
         
     def dequeue(self) -> T:
-            
-        """ Write Dis """
-            
+        """
+            This function should return the next element in the queue
+            If there is no element, throw an error. This should be O(N) in the worst
+            and average cases and O(1) in best case
+        """
         pass
         
     def peek(self) -> T:
-            
-        """ Write Dis """
-            
+        """
+            This function should return the next element in the queue.
+            This should NOT remove any elements in the queue.
+            If there is no element, throw an error. This should be O(N) in the worst
+            and average cases and O(1) in best case
+        """
         pass
 
 
     
+class Person:
+    def __init__(self, name: str) -> None:
+        self.name = name
 
-    
+
+class StoreLineLinkedList(QueueLinkedList[Person]):
+    pass
+
+class StoreLineArray(QueueArray[Person]):
+    pass
+
+class StoreLineDoubleStack(QueueDoubleStack[Person]):
+    pass
